0f27f9bae57922e6d412d311cc7c208f8635ada2
Shashank Sharma <shashanksharma.er104@gmail.com>
2025-02-21 17:49:06 +0530

    Final commit for fundamental statements

--- sql_fundamentals.sql
+++ sql_fundamentals.sql
@@ -63,6 +63,50 @@ ORDER BY length ASC
 LIMIT 5;
 
 SELECT COUNT(title) FROM film
-WHERE length <= 50
+WHERE length <= 50;
 
 --BETWEEN operator
+SELECT * FROM payment
+WHERE payment_date BETWEEN '2007-02-14' AND '2007-02-15';
+
+SELECT count(*) FROM payment
+WHERE amount BETWEEN 8 and 9;
+
+--IN operator
+SELECT * FROM payment
+WHERE customer_id IN (341, 342);
+
+SELECT count(*) FROM payment
+WHERE amount NOT IN(0.99, 1.98, 1.99);
+
+SELECT * FROM customer
+WHERE first_name IN ('John', 'Jake', 'Julie');
+
+SELECT * FROM customer
+WHERE first_name = 'John' OR first_name = 'Jake' OR first_name = 'Julie';
+
+--LIKE and ILIKE operator
+SELECT * FROM customer
+WHERE first_name LIKE 'J%' AND last_name LIKE 'J%'
+LIMIT 10;
+
+SELECT * FROM customer
+WHERE first_name LIKE 'A%' AND last_name NOT ILIKE 'B%'
+ORDER BY last_name;
+
+SELECT COUNT(*) FROM payment
+WHERE amount > 5.00;
+
+SELECT COUNT(*) FROM actor
+WHERE first_name LIKE 'P%';
+
+SELECT COUNT(DISTINCT(district)) FROM address;
+
+SELECT DISTINCT(district) FROM address;
+
+SELECT COUNT(*) FROM film
+WHERE rating = 'R' 
+AND replacement_cost BETWEEN 5 AND 15;
+
+SELECT COUNT(*) FROM film
+WHERE title LIKE '%Truman%';
