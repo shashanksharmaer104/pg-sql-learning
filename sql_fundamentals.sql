dc3a03251e7b5c5c30be63a80dd604166cb64864
Shashank Sharma <shashanksharma.er104@gmail.com>
2025-02-21 16:40:45 +0530

    Added few more fundamental commands

--- sql_fundamentals.sql
+++ sql_fundamentals.sql
@@ -36,3 +36,33 @@ WHERE title = 'Outlaw Hanky';
 
 SELECT phone FROM address
 WHERE address = '259 Ipoh Drive';
+
+--ORDER BY statement
+SELECT * FROM customer
+ORDER BY store_id;
+
+SELECT * FROM customer
+ORDER BY store_id DESC, first_name ASC;
+
+--LIMIT statement
+SELECT * FROM payment
+ORDER BY payment_date DESC
+LIMIT 5
+
+SELECT payment_id, customer_id, staff_id, amount FROM payment
+WHERE amount != 0.00
+ORDER BY payment_date DESC
+LIMIT 10
+
+SELECT customer_id FROM payment
+ORDER BY payment_date ASC
+LIMIT 10;
+
+SELECT title, length FROM film
+ORDER BY length ASC
+LIMIT 5;
+
+SELECT COUNT(title) FROM film
+WHERE length <= 50
+
+--BETWEEN operator
