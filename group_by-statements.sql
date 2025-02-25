d939f53329042cdec9b79b907af0493284362634
Shashank Sharma <shashanksharma.er104@gmail.com>
2025-02-25 16:23:30 +0530

    Added HAVING clause code

--- group_by-statements.sql
+++ group_by-statements.sql
@@ -52,3 +52,20 @@ FROM payment
 GROUP BY customer_id
 ORDER BY SUM(amount) DESC
 LIMIT 5;
+
+-- HAVING clause
+SELECT customer_id, SUM(amount) 
+FROM payment
+GROUP BY customer_id
+HAVING SUM(amount) > 100;
+
+SELECT customer_id, COUNT(customer_id)
+FROM payment
+GROUP BY customer_id
+HAVING COUNT(customer_id) >= 40;
+
+SELECT customer_id, SUM(amount)
+FROM payment
+WHERE staff_id = 2
+GROUP BY customer_id
+HAVING SUM(amount) > 100;
