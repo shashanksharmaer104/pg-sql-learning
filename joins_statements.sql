4387a03070f22354eabc9535a2f9ef27c056f4d9
Shashank Sharma <shashanksharma.er104@gmail.com>
2025-09-07 21:17:01 +0530

    Adding old changes

--- joins_statements.sql
+++ joins_statements.sql
@@ -1,7 +1,13 @@
--- AS clause
+--AS clause
 SELECT customer_id, SUM(amount) AS total_spent
 FROM payment
 GROUP BY customer_id
 HAVING SUM(amount) > 100;
 
 --INNER JOIN
+SELECT payment_id, payment.customer_id, first_name, last_name, email
+FROM payment
+INNER JOIN customer
+ON payment.customer_id = customer.customer_id;
+
+--OUTER JOIN
